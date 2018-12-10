cdb68fa56f5fc61930d5fcdbee11680bcd11a9b1
Igor Kharchenko <hatand@bk.ru>
2018-12-10 23:47:48 +0700

    Add fixtures for contact

---
+++ tests/fixtures/data/contact.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [
+        'id'      => 1,
+        'type'    => 'Skype',
+        'content' => 'irina_123',
+    ],
+    [
+        'id'      => 2,
+        'type'    => 'Instagram',
+        'content' => '@irishka12',
+    ],
+    [
+        'id'      => 3,
+        'type'    => 'YouTube',
+        'content' => 'irenaksnrg12',
+    ],
+    [
+        'id'      => 4,
+        'type'    => 'Email',
+        'content' => 'irishka331@gmail.com',
+    ],
+];
